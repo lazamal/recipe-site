5c8168cde820ad8352a9e3238c2e5be4272a35ce
Dor Lazarov <lazamal@gmail.com>
2025-05-07 12:29:36 +0300

    Initial push - overwrite remote

--- japan_foods/views.py
+++ japan_foods/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from japan_foods.models import Food
+from .forms import FoodForm
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
-def index(request):
-    japan_foods_queryset = Food.objects.all()
-    temp_name = 'japan_foods/index.html'
-    context = {
-        'foods':japan_foods_queryset
 
-    }
  
-    return render(request = request, template_name = temp_name, context = context)
+
+def add_post(request):
+    if request.method == "GET":
+        temp_name = 'japan_foods/add_post.html'
+        return render(request = request, template_name= temp_name)
+    elif request.method == "POST":
+        form = FoodForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('japan_foods:index')
+        return redirect('japan_foods:index') 
+    
+
+def search_food(request):
+    query = request.GET.get('q')
+    temp_name = 'japan_foods/index.html'
+    results = Food.objects.all()
+    
+    if query:
+        results = Food.objects.filter(food_name__icontains=query)
+    
+    context = {
+        'menu':results,
+        'query':query,
+    }
+
+    return render(request, temp_name, context= context)
+
+# class SearchResultsView(ListView):
+#     model = Food
+#     template_name = 'search_results.html'
+#     context_object_name = 'foods'
+
+#     def get_queryset(self):
+#         query = self.request.GET.get('q')
+        
+#         return Food.objects.filter(food_name__icontains=query)
+
+
+   
+
+
+
